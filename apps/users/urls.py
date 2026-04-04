9d341f1a58fd88c702a01154dc00a85d3549893b
OlegRMW <zagudpev@mail.ru>
2026-04-04 17:02:29 +0300

    put refresh token in httpOnly cookie

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -4,7 +4,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('login/', TokenObtainPairView.as_view(), name='login'),
-    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/', CookieTokenObtainPairView.as_view(), name='login'),
+    path('refresh/', CookieTokenRefreshView.as_view(), name='token_refresh'),
     path('me/', MeView.as_view(), name='me'),
 ]
